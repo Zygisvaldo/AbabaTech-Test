3fb199cd9bed6f8a507350a199a184ee845e0df9
Zygisvaldo <zygiodeze@gmail.com>
2023-09-15 16:48:19 +0200

    fix: backend cleanup

--- AbabaTech-Test/backend0/src/main.ts
+++ AbabaTech-Test/backend0/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpException, HttpStatus } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use('*', (req, res, next) => {
-  //   throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-  // });
 
   app.enableCors({
     origin: 'http://localhost:3001',
