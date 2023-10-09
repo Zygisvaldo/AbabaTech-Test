95201c44e28085303b712c5f326b79ae1299c19a
Zygisvaldo <zygiodeze@gmail.com>
2023-10-09 14:07:21 +0200

    fix: create movie dto updated with validation

--- AbabaTech-Test/backend0/src/movies/dto/create-movie.dto.ts
+++ AbabaTech-Test/backend0/src/movies/dto/create-movie.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class CreateMovieDto {
   @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
   title: string;
 
   @IsNotEmpty()
+  @MinLength(10)
+  @MaxLength(200)
   description: string;
 }
--- AbabaTech-Test/backend0/src/movies/dto/queryParams.dto.ts
+++ AbabaTech-Test/backend0/src/movies/dto/queryParams.dto.ts
@@ -8,15 +8,11 @@ export class QueryParamsDto {
 
   @IsOptional()
   @IsString()
-  @IsEnum(OrderType, {
-    message: `Invalid 'order' value. Use 'asc' or 'desc'.`,
-  })
+  @IsEnum(OrderType)
   order?: OrderType;
 
   @IsOptional()
   @IsString()
-  @IsEnum(OrderBy, {
-    message: `Invalid 'orderBy' value. Use 'title' or 'description'.`,
-  })
+  @IsEnum(OrderBy)
   orderBy?: string;
 }
